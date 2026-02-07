453888871fe18a835ac951c82551b0390923e7c4
meryemsh <meryem20030208@gmail.com>
2026-02-07 21:59:36 +0100

    the starting

--- ex3/GameEngine.py
+++ ex3/GameEngine.py
@@ -3,13 +3,29 @@ from ex3.GameStrategy import GameStrategy
 
 
 class GameEngine():
+    def __init__(self):
+        self.factory = None
+        self.strategy = None
+        self.turns = 0
+        self.total_damage = 0
+        self.cards_created = 0
+
     def configure_engine(
             self, factory: CardFactory, strategy: GameStrategy
             ) -> None:
-        pass
+        self.factory = factory
+        self.strategy = strategy
 
     def simulate_turn(self) -> dict:
-        pass
+        hand = [
+            self.factory.create_creature(),
+            self.factory.create_creature(),
+            self.factory.create_spell(),
+        ]
 
+        self.cards_created += len(hand)
+
+        result = self.strategy.execute_turn(hand, [])
     def get_engine_status(self) -> dict:
         pass
+fuck you
