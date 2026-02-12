7eeb5773b46d7a3eaf3e7a386f5d89da3c6babb2
meryemsh <meryem20030208@gmail.com>
2026-02-12 16:09:01 +0100

    final

--- ex2/Combatable.py
+++ ex2/Combatable.py
@@ -3,14 +3,14 @@ from abc import ABC, abstractmethod
 
 class Combatable(ABC):
 
-    @abstractmethod
     def attack(self, target) -> dict:
         ...
+    attack = abstractmethod(attack)
 
-    @abstractmethod
     def defend(self, incoming_damage: int) -> dict:
         ...
+    defend = abstractmethod(defend)
 
-    @abstractmethod
     def get_combat_stats(self) -> dict:
         ...
+    get_combat_stats = abstractmethod(get_combat_stats)
